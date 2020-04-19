39b5162662588bac21be373d9da9bd87ac50d8a7
Kevin Wang <kevin@xorkevin.com>
2020-04-19 14:20:44 -0700

    Add kustomization generator

--- mk/postgres/server.sh
+++ mk/postgres/server.sh
@@ -20,6 +20,25 @@ if [ -z $(check_secret $ns $secret) ]; then
     1>&2;
 fi
 
+<<EOF
+apiVersion: 'kustomize.config.k8s.io/v1beta1'
+kind: 'Kustomization'
+namespace: ${ns}
+namePrefix: ${dbname}-
+commonLabels:
+  app.kubernetes.io/name: 'postgres'
+  app.kubernetes.io/instance: 'postgres'
+  app.kubernetes.io/part-of: 'postgres'
+  app.kubernetes.io/managed-by: 'launchcpx'
+bases:
+  - 'github.com/xorkevin/launchcpx/mk/postgres/base'
+EOF
+
 if [ ! -z $configdir ]; then
-  echo "has config"
+<<EOF
+configMapGenerator:
+- name: my-application-properties
+  files:
+  - application.properties
+EOF
 fi
