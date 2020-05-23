54f323ed09f1696961f16b2cab8c57bbfa976191
Kevin Wang <kevin@xorkevin.com>
2020-05-23 16:46:45 -0700

    Impl plan register connect nats

---
+++ base/nats/init.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+export ROOT_DIR=${0%/*}
+
+. "${ROOT_DIR}/_init_lib.sh"
+
+log2 'begin genpass'
+
+pass=$(gen_pass "${PASS_LEN:-64}")
+log2 'generate password'
+
+while true; do
+  export VAULT_TOKEN=
+  i=0
+  while [ $i -lt "${CURL_REAUTH:-12}" ]; do
+    if [ -z $VAULT_TOKEN ]; then
+      export VAULT_TOKEN=$(auth_vault)
+      log2 'authenticate with vault'
+    fi
+
+    status=$(vault_kvput "$KV_PATH" "{\"password\": \"${pass}\"}")
+    if is_success "$status"; then
+      log2 'write password to vault kv'
+      break 2
+    fi
+    log2 'error write password to vault kv:' "$(cat /tmp/curlres.txt)"
+
+    i=$((i + 1))
+    sleep "${CURL_BACKOFF:-5}"
+  done
+done
+
+cp /etc/nats/server.conf /etc/nats-pass/server.conf
+cat <<EOF > /etc/nats-pass/auth.conf
+TOKEN: "${pass}"
+EOF
+log2 'write password to nats-conf'
